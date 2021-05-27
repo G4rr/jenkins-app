207089e04b67d357ae090c07bed2ae9bc9f158d3
Oleksii Pryshchepa <olexiiprish@gamil.com>
2021-05-28 01:39:49 +0300

    Reset a picture

--- application.py
+++ application.py
@@ -19,97 +19,25 @@ handler.setFormatter(formatter)
 logger.addHandler(handler)
 
 welcome = """
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<!DOCTYPE html">
 <html>
 <head>
-  <!--
-    Copyright 2012 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-
-    Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
-
-        http://aws.Amazon/apache2.0/
-
-    or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
-  -->
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <title>Welcome</title>
-  <style>
-  body {
-    color: #ffffff;
-    background-color: #E055E0;
-    font-family: Arial, sans-serif;
-    font-size:14px;
-    -moz-transition-property: text-shadow;
-    -moz-transition-duration: 4s;
-    -webkit-transition-property: text-shadow;
-    -webkit-transition-duration: 4s;
-    text-shadow: none;
-  }
-  body.blurry {
-    -moz-transition-property: text-shadow;
-    -moz-transition-duration: 4s;
-    -webkit-transition-property: text-shadow;
-    -webkit-transition-duration: 4s;
-    text-shadow: #fff 0px 0px 25px;
-  }
-  a {
-    color: #0188cc;
-  }
-  .textColumn, .linksColumn {
-    padding: 2em;
-  }
-  .textColumn {
-    position: absolute;
-    top: 0px;
-    right: 50%;
-    bottom: 0px;
-    left: 0px;
-
-    text-align: right;
-    padding-top: 11em;
-    background-color: #00A86D;
-    background-image: -moz-radial-gradient(left top, circle, #6AF9BD 0%, #00B386 60%);
-    background-image: -webkit-gradient(radial, 0 0, 1, 0 0, 500, from(#6AF9BD), to(#00B386));
-  }
-  .textColumn p {
-    width: 75%;
-    float:right;
-  }
-  .linksColumn {
-    position: absolute;
-    top:0px;
-    right: 0px;
-    bottom: 0px;
-    left: 50%;
-
-    background-color: #E0E0E0;
-  }
-
-  h1 {
-    font-size: 500%;
-    font-weight: normal;
-    margin-bottom: 0em;
-  }
-  h2 {
-    font-size: 200%;
-    font-weight: normal;
-    margin-bottom: 0em;
-  }
-  ul {
-    padding-left: 1em;
-    margin: 0px;
-  }
-  li {
-    margin: 1em 0em;
-  }
-  </style>
+  <title>Welcome to my website</title>
+  <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Lobster+Two" type="text/css">
+    <link rel="icon" href="https://awsmedia.s3.amazonaws.com/favicon.ico" type="image/ico" >
+    <link rel="shortcut icon" href="https://awsmedia.s3.amazonaws.com/favicon.ico" type="image/ico" >
+    <!--[if IE]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+    <link rel="stylesheet" href="/styles.css" type="text/css">
 </head>
 <body id="sample">
   <div class="textColumn">
     <h1>Congratulations my friend</h1>
-    <p>Your first AWS Elastic Beanstalk Python Application is now running on your own dedicated environment in the AWS Cloud</p>
-    <p>This environment is launched with Elastic Beanstalk Python Platform</p>
-    <img src="https://github.com/G4rr/jenkins-app/blob/master/image.jpg">
+    <p>Meme, unit of cultural information spread by imitation. The term meme (from the Greek mimema, meaning “imitated”) 
+    was introduced in 1976 by British evolutionary biologist Richard Dawkins in his work The Selfish Gene.</p>
+    <p>TDawkins conceived of memes as the cultural parallel to biological genes and considered them, in a manner similar 
+    to “selfish” genes, as being in control of their own reproduction and thus serving their own ends.</p>
+    <img src="image.jpg">
   </div>
   
   <div class="linksColumn"> 
