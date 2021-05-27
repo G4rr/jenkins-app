f3fa5222a5e08b6da356a011d18ff7ec4f362279
Oleksii Pryshchepa <olexiiprish@gamil.com>
2021-05-28 01:05:30 +0300

    Add image

--- application.py
+++ application.py
@@ -109,6 +109,7 @@ welcome = """
     <h1>Congratulations</h1>
     <p>Your first AWS Elastic Beanstalk Python Application is now running on your own dedicated environment in the AWS Cloud</p>
     <p>This environment is launched with Elastic Beanstalk Python Platform</p>
+    <img src="image.jpg">
   </div>
   
   <div class="linksColumn"> 
