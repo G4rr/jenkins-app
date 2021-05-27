403f2bd5e807286ff6f41f9a2052c9d0d0b8131a
Pryshchepa Oleksii <73528295+G4rr@users.noreply.github.com>
2021-05-28 00:40:01 +0300

    Update application.py

--- application.py
+++ application.py
@@ -106,13 +106,13 @@ welcome = """
 </head>
 <body id="sample">
   <div class="textColumn">
-    <h1>Congratulations</h1>
+    <h1>Congratulations my friend</h1>
     <p>Your first AWS Elastic Beanstalk Python Application is now running on your own dedicated environment in the AWS Cloud</p>
     <p>This environment is launched with Elastic Beanstalk Python Platform</p>
   </div>
   
   <div class="linksColumn"> 
-    <h2>What's Next?</h2>
+    <h2>The Next Links</h2>
     <ul>
     <li><a href="http://docs.amazonwebservices.com/elasticbeanstalk/latest/dg/">AWS Elastic Beanstalk overview</a></li>
     <li><a href="http://docs.amazonwebservices.com/elasticbeanstalk/latest/dg/index.html?concepts.html">AWS Elastic Beanstalk concepts</a></li>
